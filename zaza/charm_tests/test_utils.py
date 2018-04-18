8004532cfdcda2bdf2e82cd5f665a3d9c8e5c769
Frode Nordahl <frode.nordahl@canonical.com>
2018-04-18 12:22:00 +0200

    Add get_actions helper function
    Note that this helper function should be removed as soon
    as it is implemented in libjuju.

--- zaza/charm_tests/test_utils.py
+++ zaza/charm_tests/test_utils.py
@@ -1,4 +1,6 @@
+import json
 import logging
+import subprocess
 import zaza.model
 
 import zaza.charm_lifecycle.utils as utils
@@ -16,3 +18,22 @@ def skipIfNotHA(service_name):
         return _skipIfNotHA_inner_2
 
     return _skipIfNotHA_inner_1
+
+
+# NOTE(fnordahl) this function exists because libjuju has currently not
+#                implemented Application.get_actions()
+def get_actions(application, schema=False):
+    """
+    Get available actions for application, optionally get schema (schema==True)
+
+    :param schema: If set, include action schema in the returned data.
+    :type schema: Optional[bool]
+    :returns: Dictionary where keys are action names.
+    :rtype: dict
+    """
+    logging.debug("list actions for application '{}' schema='{}'"
+                  "".format(application, schema))
+    cmd = ['juju', 'list-actions', "--schema={}".format(schema),
+           '--format=json', application]
+    actions = json.loads(subprocess.check_output(cmd))
+    return actions
