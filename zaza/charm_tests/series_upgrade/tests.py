12ab3d8a406a674096caac5c60af537fc618ee5f
David Ames <david.ames@canonical.com>
2018-10-16 15:32:51 -0700

    Handle series upgrade for memcached

--- zaza/charm_tests/series_upgrade/tests.py
+++ zaza/charm_tests/series_upgrade/tests.py
@@ -70,6 +70,10 @@ class SeriesUpgradeTest(unittest.TestCase):
                 origin = "source"
                 pause_non_leader_primary = False
                 pause_non_leader_subordinate = False
+            if "memcached" in applications[application]["charm"]:
+                origin = None
+                pause_non_leader_primary = False
+                pause_non_leader_subordinate = False
             if "mongodb" in applications[application]["charm"]:
                 # Mongodb is sligthly different. It needs to run series upgrade
                 # on its secondaries first. It has its own function.
