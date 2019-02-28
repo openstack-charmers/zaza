aadaff8d09697fd120242859be7739d50d82b2a4
Chris MacNaughton <chris@centaurisolutions.nl>
2019-02-28 14:32:08 +0100

    Add ceph-osd test

--- zaza/charm_tests/ceph/osd/__init__.py
+++ zaza/charm_tests/ceph/osd/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Collection of code for setting up and testing ceph."""
+"""Collection of code for setting up and testing ceph-osd."""
